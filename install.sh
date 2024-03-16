038c6aaa1c5c5b4b46001768e5fcf37c2d6c7e5c
CodeProSpecialist <droark28@hotmail.com>
2024-03-16 16:33:40 -0500

    Code updates

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Install dependencies using apt-get
 echo "Installing dependencies using apt-get..."
 sudo apt-get update
-sudo apt-get install -y python3-pip libta-lib0
+sudo apt-get install -y python3-pip
 
 # Install dependencies using pip3
 echo "Installing dependencies using pip3..."
