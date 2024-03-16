80620a6887a14210c8db7971b11378fe74727540
CodeProSpecialist <droark28@hotmail.com>
2024-03-16 17:29:35 -0500

    Code updates

--- python-stock-market-scanner.py
+++ python-stock-market-scanner.py
@@ -27,24 +27,24 @@ def analyze_stock(symbol):
     start_date_90_days_ago = (datetime.today() - timedelta(days=90)).strftime('%Y-%m-%d')
     start_date_8_days_ago = (datetime.today() - timedelta(days=8)).strftime('%Y-%m-%d')
 
-    # Fetch current price
-    current_price = yf.Ticker(symbol).history(period='1d')['Close'].iloc[-1]
-
     data_90_days = get_data(symbol, start_date_90_days_ago, end_date).values
     data_8_days = get_data(symbol, start_date_8_days_ago, end_date).values
 
     rsi_90_days, macd_90_days, _ = calculate_indicators(data_90_days)
     rsi_8_days, macd_8_days, macd_signal_8_days = calculate_indicators(data_8_days)
 
+    current_volume = data_8_days[-1, 5]
+    average_volume = np.mean(data_90_days[:, 5])
+
     # Conditions for recommending a stock
-    if (data_8_days[-1, 5] > np.mean(data_90_days[:, 5]) * 1.5) and \
+    if (current_volume >= 0.85 * average_volume) and \
             (rsi_8_days[-1] > 50) and \
             (macd_8_days[-1] > macd_signal_8_days[-1]) and \
             (data_8_days[-1, 4] > data_90_days[-1, 4]) and \
             (data_8_days[-1, 4] > data_8_days[-2, 4]):
-        return True, round(data_8_days[-1, 4], 2), round(data_8_days[-1, 5], 2), round(current_price, 2)
+        return True, round(data_8_days[-1, 4], 2), current_volume, average_volume, rsi_8_days[-1], macd_8_days[-1]
     else:
-        return False, round(data_8_days[-1, 4], 2), round(data_8_days[-1, 5], 2), round(current_price, 2)
+        return False, round(data_8_days[-1, 4], 2), current_volume, average_volume, rsi_8_days[-1], macd_8_days[-1]
 
 
 def get_next_run_time():
@@ -79,11 +79,13 @@ def main():
                 etfs = ['SPY', 'QQQ', 'SPXL', 'VTI', 'VGT']
 
                 for etf in etfs:
-                    recommended, close_price, volume, current_price = analyze_stock(etf)
+                    recommended, close_price, current_volume, average_volume, rsi, macd = analyze_stock(etf)
                     print(f"\nAnalysis for {etf}:")
                     print(f"Close Price: {close_price:.2f}")
-                    print(f"Current Price: {current_price:.2f}")
-                    print(f"Volume: {volume:.2f}")
+                    print(f"Current Volume: {current_volume:.2f}")
+                    print(f"Average Volume: {average_volume:.2f}")
+                    print(f"RSI: {rsi:.2f}")
+                    print(f"MACD: {macd:.2f}")
 
                     if recommended:
                         print(f"{etf} is recommended to buy today.")
