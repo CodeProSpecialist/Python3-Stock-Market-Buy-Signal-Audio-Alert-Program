0c8aa652e9b4b99ffe724103f557c2cc0b85e066
CodeProSpecialist <111866070+CodeProSpecialist@users.noreply.github.com>
2024-03-16 10:17:47 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+# A Shell script
+
+# Install dependencies using apt-get
+echo "Installing dependencies using apt-get..."
+sudo apt-get update
+sudo apt-get install -y python3-pip libta-lib0
+
+# Install dependencies using pip3
+echo "Installing dependencies using pip3..."
+pip3 install yfinance TA-Lib
