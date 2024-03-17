1c6cbf80278e2f91fe1bec92b3b0cae8706d6baf
CodeProSpecialist <111866070+CodeProSpecialist@users.noreply.github.com>
2024-03-17 12:34:29 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
 #/bin/sh
 
+# Ubuntu Linux install commands
+
 # Update apt-get package lists
 sudo apt-get update
 
 # Install Python 3.10 and pip3
 sudo apt-get install -y python3.10-full python3-pip
+
+# Install sound dependencies
+sudo apt-get install -y espeak
 
 # Install yfinance and TA-Lib using pip3
 pip3 install yfinance TA-Lib
