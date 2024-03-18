d50533f5c2be0147a4af0f3c71e122a614744ebc
CodeProSpecialist <droark28@hotmail.com>
2024-03-18 10:26:37 -0500

    code updates

--- python-stock-market-scanner.py
+++ python-stock-market-scanner.py
@@ -1,83 +1,85 @@
 import subprocess
 import yfinance as yf
+from datetime import datetime, timedelta
 import numpy as np
+from talib import RSI, MACD
 import pytz
 import time
-import talib
-from datetime import datetime, timedelta
 
-def get_opening_price(symbol):
-    stock_data = yf.Ticker(symbol)
-    return round(stock_data.history(period="1d")["Open"].iloc[0], 4)
 
-def get_closing_price(symbol):
-    stock_data = yf.Ticker(symbol)
-    return round(stock_data.history(period="1d")["Close"].iloc[-1], 4)
+def get_data(symbol, start_date, end_date):
+    try:
+        data = yf.download(symbol, start=start_date, end=end_date)
+        return data
+    except Exception as e:
+        print(f"Failed to download data for {symbol}: {e}")
+        return None
 
-def get_current_price(symbol):
-    stock_data = yf.Ticker(symbol)
-    return round(stock_data.history(period='1d')['Close'].iloc[0], 4)
 
-def get_yesterday_closing_price(symbol):
-    stock_data = yf.Ticker(symbol)
-    return round(stock_data.history(period='1d')['Close'].iloc[-2], 4)
+def calculate_indicators(data):
+    close_prices = data[:, 4]
+    rsi = RSI(close_prices, timeperiod=14)
+    macd, macd_signal, _ = MACD(close_prices, fastperiod=12, slowperiod=26, signalperiod=9)
+    return rsi, macd, macd_signal
 
-def calculate_technical_indicators(symbol, lookback_days=180):
-    stock_data = yf.Ticker(symbol)
-    historical_data = stock_data.history(period=f'{lookback_days}d')
-
-    short_window = 12
-    long_window = 26
-    signal_window = 9
-    historical_data['macd'], historical_data['signal'], _ = talib.MACD(historical_data['Close'],
-                                                                       fastperiod=short_window,
-                                                                       slowperiod=long_window,
-                                                                       signalperiod=signal_window)
-
-    rsi_period = 14
-    historical_data['rsi'] = talib.RSI(historical_data['Close'], timeperiod=rsi_period)
-
-    historical_data['volume'] = historical_data['Volume']
-
-    return historical_data
 
 def analyze_stock(symbol):
     end_date = datetime.today().strftime('%Y-%m-%d')
     start_date_6_months_ago = (datetime.today() - timedelta(days=180)).strftime('%Y-%m-%d')
+    start_date_1_day_ago = (datetime.today() - timedelta(days=1)).strftime('%Y-%m-%d')
 
-    data_6_months = get_data(symbol, start_date_6_months_ago, end_date).values
+    data_6_months = get_data(symbol, start_date_6_months_ago, end_date)
+    data_1_day = get_data(symbol, start_date_1_day_ago, end_date)
 
-    open_price = get_opening_price(symbol)
-    current_price = get_current_price(symbol)
-    yesterday_close_price = get_yesterday_closing_price(symbol)
-    current_volume = data_6_months[-1, 5]
+    if data_6_months is None or data_1_day is None:
+        return False, None, None, None, None, None, None, None
+
+    current_close_price = data_1_day[-1, 3]
+    current_open_price = data_1_day[-1, 0]
+    current_price = get_current_price(symbol)  # Get today's current price
+    current_volume = data_1_day[-1, 5]
     average_volume = np.mean(data_6_months[:, 5])
 
-    historical_data = calculate_technical_indicators(symbol)
+    rsi_6_months, macd_6_months, _ = calculate_indicators(data_6_months)
+    rsi_1_day, macd_1_day, macd_signal_1_day = calculate_indicators(data_1_day)
 
-    rsi = round(historical_data['rsi'].iloc[-1], 2)
-    macd = round(historical_data['macd'].iloc[-1], 2)
-
-    if (current_price > open_price) and \
-            (current_price > yesterday_close_price) and \
+    if (current_open_price > current_close_price) and \
             ((current_volume > average_volume) or (current_volume >= 0.9 * average_volume)) and \
-            (rsi > 55):
-        return True, open_price, current_price, yesterday_close_price, current_volume, average_volume, rsi, macd
+            (rsi_1_day[-1] > 55) and \
+            (macd_1_day[-1] > macd_signal_1_day[-1]):
+        return True, round(current_close_price, 2), round(current_open_price, 2), round(current_price,
+                                                                                        2), current_volume, average_volume, round(
+            rsi_1_day[-1], 2), round(macd_1_day[-1], 2)
     else:
-        return False, open_price, current_price, yesterday_close_price, current_volume, average_volume, rsi, macd
+        return False, round(current_close_price, 2), round(current_open_price, 2), round(current_price,
+                                                                                         2), current_volume, average_volume, round(
+            rsi_1_day[-1], 2), round(macd_1_day[-1], 2)
+
+
+def get_current_price(symbol):
+    stock_data = yf.Ticker(symbol)
+    try:
+        return round(stock_data.history(period='1d')['Close'].iloc[0], 4)
+    except Exception as e:
+        print(f"Failed to get current price for {symbol}: {e}")
+        return None
+
 
 def get_next_run_time():
     eastern = pytz.timezone('US/Eastern')
     now = datetime.now()
     next_run_time = now.replace(hour=10, minute=15, second=0, microsecond=0)
 
-    if now > next_run_time:
+    # If the next run time is past 4 PM, schedule it for the next day
+    if now.hour >= 16:
         next_run_time += timedelta(days=1)
 
+    # Check if the next run time falls on a weekend, if so, advance to Monday
     while next_run_time.weekday() >= 5:
         next_run_time += timedelta(days=1)
 
     return eastern.localize(next_run_time)
+
 
 def main():
     eastern = pytz.timezone('US/Eastern')
@@ -95,26 +97,33 @@ def main():
                 etfs = ['SPY', 'QQQ', 'SPXL', 'VTI', 'VGT']
 
                 for etf in etfs:
-                    recommended, open_price, current_price, yesterday_close_price, current_volume, average_volume, rsi, macd = analyze_stock(etf)
-                    print(f"\nAnalysis for {etf}:")
-                    print(f"Yesterday's Close Price: {yesterday_close_price:.2f}")
-                    print(f"Open Price for Today: {open_price:.2f}")
-                    print(f"Current Price: {current_price:.2f}")
-                    print(f"Current Volume: {current_volume:.2f}")
-                    print(f"Average Volume: {average_volume:.2f}")
-                    print(f"RSI: {rsi}")
-                    print(f"MACD: {macd}")
+                    recommended, close_price, open_price, current_price, current_volume, average_volume, rsi, macd = analyze_stock(
+                        etf)
 
-                    if recommended:
-                        print(f"{etf} is recommended to buy today.")
-                        subprocess.run(["espeak", f"Time to buy {etf} right now."])
+                    if recommended is not None:
+                        print(f"\nAnalysis for {etf}:")
+                        print(f"Yesterday's Close Price: {close_price:.2f}")
+                        print(f"Open Price for Today: {open_price:.2f}")
+                        print(f"Current Price: {current_price:.2f}")  # Print today's current price
+                        print(f"Current Volume: {current_volume:.2f}")
+                        print(f"Average Volume: {average_volume:.2f}")
+                        print(f"RSI: {rsi}")
+                        print(f"MACD: {macd}")
+
+                        if recommended:
+                            print(f"{etf} is recommended to buy today.")
+                            subprocess.run(["espeak", f"Time to buy {etf} right now."])
+                        else:
+                            print(f"{etf} is not recommended to buy today.")
                     else:
-                        print(f"{etf} is not recommended to buy today.")
+                        print(f"No data available for {etf}")
 
                 next_run_time += timedelta(seconds=30)
-                print("\nNext Run Time:", next_run_time.astimezone(eastern).strftime("%Y-%m-%d %I:%M:%S %p"), "Eastern Time ")
+                print("\nNext Run Time:", next_run_time.astimezone(eastern).strftime("%Y-%m-%d %I:%M:%S %p"),
+                      "Eastern Time ")
 
         time.sleep(30)
+
 
 if __name__ == "__main__":
     while True:
