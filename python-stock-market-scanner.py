99c58b1cc05a0262f26c05cb75e4da582cd77ed3
CodeProSpecialist <droark28@hotmail.com>
2024-03-18 10:30:23 -0500

    code updates

--- python-stock-market-scanner.py
+++ python-stock-market-scanner.py
@@ -6,7 +6,6 @@ from talib import RSI, MACD
 import pytz
 import time
 
-
 def get_data(symbol, start_date, end_date):
     try:
         data = yf.download(symbol, start=start_date, end=end_date)
@@ -15,13 +14,27 @@ def get_data(symbol, start_date, end_date):
         print(f"Failed to download data for {symbol}: {e}")
         return None
 
+def get_opening_price(symbol):
+    stock_data = yf.Ticker(symbol)
+    try:
+        return round(stock_data.history(period="1d")["Open"].iloc[0], 4)
+    except Exception as e:
+        print(f"Failed to get opening price for {symbol}: {e}")
+        return None
+
+def get_current_price(symbol):
+    stock_data = yf.Ticker(symbol)
+    try:
+        return round(stock_data.history(period='1d')['Close'].iloc[0], 4)
+    except Exception as e:
+        print(f"Failed to get current price for {symbol}: {e}")
+        return None
 
 def calculate_indicators(data):
     close_prices = data[:, 4]
     rsi = RSI(close_prices, timeperiod=14)
     macd, macd_signal, _ = MACD(close_prices, fastperiod=12, slowperiod=26, signalperiod=9)
     return rsi, macd, macd_signal
-
 
 def analyze_stock(symbol):
     end_date = datetime.today().strftime('%Y-%m-%d')
@@ -34,52 +47,36 @@ def analyze_stock(symbol):
     if data_6_months is None or data_1_day is None:
         return False, None, None, None, None, None, None, None
 
-    current_close_price = data_1_day[-1, 3]
-    current_open_price = data_1_day[-1, 0]
-    current_price = get_current_price(symbol)  # Get today's current price
-    current_volume = data_1_day[-1, 5]
-    average_volume = np.mean(data_6_months[:, 5])
+    current_close_price = data_1_day.iloc[-1]['Close']
+    current_open_price = get_opening_price(symbol)
+    current_price = get_current_price(symbol)
+    current_volume = data_1_day.iloc[-1]['Volume']
+    average_volume = np.mean(data_6_months['Volume'])
 
     rsi_6_months, macd_6_months, _ = calculate_indicators(data_6_months)
     rsi_1_day, macd_1_day, macd_signal_1_day = calculate_indicators(data_1_day)
 
-    if (current_open_price > current_close_price) and \
-            ((current_volume > average_volume) or (current_volume >= 0.9 * average_volume)) and \
-            (rsi_1_day[-1] > 55) and \
-            (macd_1_day[-1] > macd_signal_1_day[-1]):
-        return True, round(current_close_price, 2), round(current_open_price, 2), round(current_price,
-                                                                                        2), current_volume, average_volume, round(
-            rsi_1_day[-1], 2), round(macd_1_day[-1], 2)
+    if current_open_price is not None and current_price is not None:
+        if (current_open_price > current_close_price) and \
+                ((current_volume > average_volume) or (current_volume >= 0.9 * average_volume)) and \
+                (rsi_1_day[-1] > 55) and \
+                (macd_1_day[-1] > macd_signal_1_day[-1]):
+            return True, round(current_close_price, 2), round(current_open_price, 2), round(current_price, 2), current_volume, average_volume, round(rsi_1_day[-1], 2), round(macd_1_day[-1], 2)
     else:
-        return False, round(current_close_price, 2), round(current_open_price, 2), round(current_price,
-                                                                                         2), current_volume, average_volume, round(
-            rsi_1_day[-1], 2), round(macd_1_day[-1], 2)
-
-
-def get_current_price(symbol):
-    stock_data = yf.Ticker(symbol)
-    try:
-        return round(stock_data.history(period='1d')['Close'].iloc[0], 4)
-    except Exception as e:
-        print(f"Failed to get current price for {symbol}: {e}")
-        return None
-
+        return False, round(current_close_price, 2), round(current_open_price, 2), round(current_price, 2), current_volume, average_volume, round(rsi_1_day[-1], 2), round(macd_1_day[-1], 2)
 
 def get_next_run_time():
     eastern = pytz.timezone('US/Eastern')
     now = datetime.now()
     next_run_time = now.replace(hour=10, minute=15, second=0, microsecond=0)
 
-    # If the next run time is past 4 PM, schedule it for the next day
     if now.hour >= 16:
         next_run_time += timedelta(days=1)
 
-    # Check if the next run time falls on a weekend, if so, advance to Monday
     while next_run_time.weekday() >= 5:
         next_run_time += timedelta(days=1)
 
     return eastern.localize(next_run_time)
-
 
 def main():
     eastern = pytz.timezone('US/Eastern')
@@ -97,8 +94,7 @@ def main():
                 etfs = ['SPY', 'QQQ', 'SPXL', 'VTI', 'VGT']
 
                 for etf in etfs:
-                    recommended, close_price, open_price, current_price, current_volume, average_volume, rsi, macd = analyze_stock(
-                        etf)
+                    recommended, close_price, open_price, current_price, current_volume, average_volume, rsi, macd = analyze_stock(etf)
 
                     if recommended is not None:
                         print(f"\nAnalysis for {etf}:")
@@ -119,11 +115,9 @@ def main():
                         print(f"No data available for {etf}")
 
                 next_run_time += timedelta(seconds=30)
-                print("\nNext Run Time:", next_run_time.astimezone(eastern).strftime("%Y-%m-%d %I:%M:%S %p"),
-                      "Eastern Time ")
+                print("\nNext Run Time:", next_run_time.astimezone(eastern).strftime("%Y-%m-%d %I:%M:%S %p"), "Eastern Time ")
 
         time.sleep(30)
-
 
 if __name__ == "__main__":
     while True:
