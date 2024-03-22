e23260621156a4d349b9abf6513b7e28843198d6
CodeProSpecialist <droark28@hotmail.com>
2024-03-22 06:53:48 -0500

    code updates

--- python-stock-market-scanner.py
+++ python-stock-market-scanner.py
@@ -94,7 +94,7 @@ def main():
         if now >= next_run_time and now.hour < 16:
             if now.weekday() < 5:
                 print("Recommended Stocks to Buy Today:")
-                etfs = ['SPY', 'QQQ', 'SPXL', 'SPMD', 'VTI', 'VGT']
+                etfs = ['SPY', 'QQQ', 'SPXL', 'MIDU', 'SPMD', 'VTI', 'VGT']
 
                 for etf in etfs:
                     recommended, close_price, open_price, current_price, current_volume, average_volume, rsi, macd = analyze_stock(etf)
